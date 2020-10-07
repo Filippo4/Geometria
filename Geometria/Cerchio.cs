b29950e60fc5563a265e389e5d1aa9e078c9c285
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-10-07 12:00:05 +0200

    Add project files.

---
+++ Geometria/Cerchio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Cerchio
+    {
+        public const double PI = 3.14;
+        private double raggio;
+        public Cerchio(double r)
+        {
+            raggio = r;
+        }
+        public double CalcolaCirconferenza()
+        {
+            return 2 * PI * raggio;
+        }
+        public double CalcolaArea()
+        {
+            return PI * raggio * raggio;
+        }
+        public void CambiaRaggio(double nuovoraggio)
+        {
+            raggio = nuovoraggio;
+        }
+        public double DammiRaggio()
+        {
+            return raggio;
+        }
+        public string StampaStringa()
+        {
+            return $"cercio (r={raggio})" +
+                    $"circonferenza = {CalcolaCirconferenza()}" +
+                    $"area = {CalcolaArea()}";
+        }
+    }
+}
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Rettangolo
+    {
+        private int x;
+        private int y;
+        private double widht;
+        private double height;
+        public Rettangolo(int x, int y, double w, double h)
+        {
+            this.x = x;
+            this.y = y;
+            widht = w;
+            height = h;
+        }
+        public bool Contiene(Rettangolo r)
+        {
+            if (r.x >= this.x && r.widht + r.x <= this.widht + this.x)
+            {
+                if (r.y >= this.y && r.height + r.y <= this.height + this.y)
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+        public double CalcolaArea()
+        {
+            return widht * height;
+        }
+        public double CalcolaPerimetro()
+        {
+            return (widht + height) * 2;
+        }
+
+    }
+}
